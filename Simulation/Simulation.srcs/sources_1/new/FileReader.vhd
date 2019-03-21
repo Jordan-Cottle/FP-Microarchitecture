03be00040cc0963249b63d94af3f260c1713ad0e
JordanCottle <jordancottle622@gmail.com>
2019-03-21 12:58:09 -0400

    Added intructions and reader

--- Simulation/Simulation.srcs/sources_1/new/FileReader.vhd
+++ Simulation/Simulation.srcs/sources_1/new/FileReader.vhd
@@ -15,10 +15,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use std.textio.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -31,7 +32,60 @@ end FileReader;
 
 architecture Behavioral of FileReader is
 
+    file instructions: text;
+    file results: text;
+
 begin
 
+    process
+        variable lineIn: line;
+        variable lineOut: line;
 
+        variable operation: string(1 to 3) := "ADD";
+        variable Rd: string(1 to 2) := "R1";
+        variable Ri: string(1 to 2) := "R2";
+        variable Rj: string(1 to 2) := "R3";
+
+        variable seperator: character;
+        variable sep: string(1 to 2) := ", ";
+        
+        variable count: integer := 0;
+    begin
+        file_open(instructions, "../../../../Simulation.configuration/InputFiles/instructions.txt", read_mode);
+        file_open(results, "../../../../Simulation.configuration/OutputFiles/output.txt", write_mode);
+
+        while not(count = 10) loop -- 
+            readline(instructions, lineIn);
+            read(lineIn, operation); -- get operation
+            read(lineIn, seperator); -- read space
+            read(lineIn, Rd); -- get destination register
+            read(lineIn, seperator); -- read space
+            read(lineIn, seperator); -- read comma
+            read(lineIn, Ri); -- get first source register
+            read(lineIn, seperator);
+            read(lineIn, Rj); -- get second source register
+
+            wait for 20ns;
+
+            write(lineOut, count);
+            write(lineOut, string'(": "));
+            write(lineOut, operation);
+            write(lineOut, ' ');
+            write(lineOut, Rd);
+            write(lineOut, string'(", "));
+            write(lineOut, Ri);
+            write(lineOut, ' ');
+            write(lineOut, Rj);
+            writeline(results, lineOut);
+            
+            count := count + 1;
+
+        end loop;
+
+        file_close(instructions);
+        file_close(results);
+
+        wait;
+    end process;
+    
 end Behavioral;
