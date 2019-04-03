8f5e23e30b2a8550da040de999e7a36f171ffd4a
JordanCottle <jordancottle622@gmail.com>
2019-04-03 09:25:59 -0400

    Created for static values.
    Currently contains absolute paths to Input and Output folders for driving simulations.
    Each machine will need to update these values locally to reflect accurate paths.

---
+++ Simulation/Simulation.srcs/sim_1/new/Constants.vhd
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Jordan Cottle
+-- 
+-- Create Date: 04/03/2019 09:11:09 AM
+-- Module Name: Constants
+-- Description: Package to contain constant values and various reusable components
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+package Constants is
+    constant inputFolderPath: string := "C:\Users\jorda\Documents\Projects\FP-Microarchitecture\Simulation\Simulation.configuration\InputFiles\";
+    constant outputFolderPath: string := "C:\Users\jorda\Documents\Projects\FP-Microarchitecture\Simulation\Simulation.configuration\OutputFiles\";
+end Constants;
+
+package body Constants is
+    
+end Constants;
