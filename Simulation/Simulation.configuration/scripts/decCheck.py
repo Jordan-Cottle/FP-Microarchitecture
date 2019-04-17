7da0d81a06473cf0090130164a7ca24bd3eb9a81
JordanCottle <jordancottle622@gmail.com>
2019-04-17 14:08:22 -0400

    use relative paths and count errors

--- Simulation/Simulation.configuration/scripts/decCheck.py
+++ Simulation/Simulation.configuration/scripts/decCheck.py
@@ -1,22 +1,32 @@
-import urllib.request, json 
-from time import sleep
+from urllib import request
+from json import loads
+from os import path
 
-inputFile = open('C:\\Users\\Jordan\\Documents\\Projects\\FP-Microarchitecture\\Simulation\\Simulation.configuration\\InputFiles\\fpValues.txt', 'r')
-outputFile = open('C:\\Users\\Jordan\\Documents\\Projects\\FP-Microarchitecture\\Simulation\\Simulation.configuration\\OutputFiles\\decValues.txt', 'r')
+scriptPath = path.abspath(__file__)
+scriptDirPath = path.split(scriptPath)[0]
+configPath = path.split(scriptDirPath)[0]
+
+inputFile = open(f'{configPath}\\InputFiles\\fpValues.txt', 'r')
+outputFile = open(f'{configPath}\\OutputFiles\\decValues.txt', 'r')
+resultsFile = open(f'{configPath}\\OutputFiles\\decimalConversionTestResults.txt', 'w')
 
 count = 1
+errorCount = 0
 for line in inputFile:
-    with urllib.request.urlopen("https://www.h-schmidt.net/FloatConverter/binary-json.py?binary=" + line) as url:
-        data = json.loads(url.read().decode())
+    with request.urlopen("https://www.h-schmidt.net/FloatConverter/binary-json.py?binary=" + line) as url:
+        data = loads(url.read().decode())
         expected = float(data['decimalRepr'])
         result = float(outputFile.readline())
 
         if abs(result - expected) > abs(expected / 100000):
-            print(f'{count}: expected {expected}, got {result}')
+            print(f'{count}: expected {expected}, got {result}', file=resultsFile)
+            errorCount += 1
         else:
-            print(f'{count}: success!')
-
+            print(f'{count}: success!', file=resultsFile)
     count += 1
 
+print(f'{errorCount} errors found!', file=resultsFile)
 
 inputFile.close()
+outputFile.close()
+resultsFile.close()
