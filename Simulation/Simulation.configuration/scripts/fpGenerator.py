829986dd54b4c74f07122c223ab63140070f2d51
Jordan <jordancottle622@gmail.com>
2019-04-20 07:56:46 -0400

    Changed how fp numbers are generated

--- Simulation/Simulation.configuration/scripts/fpGenerator.py
+++ Simulation/Simulation.configuration/scripts/fpGenerator.py
@@ -5,13 +5,19 @@ scriptPath = path.abspath(__file__)
 scriptDirPath = path.split(scriptPath)[0]
 configPath = path.split(scriptDirPath)[0]
 
-fpFile = open(f'{configPath}/InputFiles/fpValues.txt', 'r')
+fpFile = open(f'{configPath}/InputFiles/fpValues.txt', 'w')
 
-
+lastExponent = 127
 bitVectors = []
-for _ in range (25000):
-    binaryString = [str(r.randint(0,1)) for _ in range(32)]
-    bitVectors.append(''.join(binaryString))
+for i in range (10000):
+    sign = str(r.randint(0,1))
+    exponent = r.randint(lastExponent - 10, lastExponent + 10) % 255
+    lastExponent = exponent
+    exponent = bin(exponent)[2:].zfill(8)
+    mantissa = ''.join([str(r.randint(0,1)) for _ in range(23)])
+
+    binaryString = ''.join([sign,exponent,mantissa])
+    bitVectors.append(binaryString)
 
 #bitVectors.sort()
 for bitVector in bitVectors:
