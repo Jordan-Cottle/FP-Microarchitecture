b218428dbcd10f86f5a55e9da2de9748bb7a22b5
JordanCottle <jordancottle622@gmail.com>
2019-04-26 16:21:36 -0400

    test bench with loop

--- Register_File/Register_File.srcs/sim_1/new/Register_File_tb.vhd
+++ Register_File/Register_File.srcs/sim_1/new/Register_File_tb.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -37,7 +37,7 @@ end Register_File_tb;
 
 architecture Behavioral of Register_File_tb is
 
-   signal out1, out2, Write : std_logic_vector(31 downto 0) := x"00000";
+   signal out1, out2, Write : std_logic_vector(31 downto 0) := x"00000000";
    signal Read_1, Read_2, Write_Val : std_logic_vector(4 downto 0) := "00000";
    signal Enable, clock : std_logic:= '0';
      
@@ -45,7 +45,7 @@ begin
 
    uut : entity work.Registers_Fmain 
    port map( Read_1,Read_2,Write,Write_Val,Enable,out1,out2,clock);
-   clkgen: process
+   process
    begin 
          clock <='1';
          wait for 1 ns;
@@ -53,31 +53,17 @@ begin
          wait for 1 ns;
    end process;
    
-   insig: process
+   process(clock)
+      variable count: integer := 0;
    begin
-        Write_Val <= x"AAAA";
-        Write <= "00001";
-        Enable <= '1';
-        wait for 2 ns;
-        Enable <= '0';
-        wait for 1 ns;
-        Write_Val <= x"EEEE";
-        Write <= "00011";
-        Enable <= '1';
-        wait for 2 ns;
-        Enable <= '0'; 
-        Write_Val <= x"0000";
-        Write <= "00000";
-        wait for 2 ns;
-        Read_1 <= "00001";
-        Read_2 <= "00011";
-        wait for 10 ns;
-        Write_Val <= x"BBBB";
-        Write <= "00011";
-        Enable <= '1';
-        wait for 2 ns;
-        Enable <= '0';
-        wait for 10 ns;
-             
-end process; 
+      enable <= '1';
+      if(clock = '0') then
+         -- pass
+      elsif 
+         write <= std_logic_vector(to_unsigned(count, 32));
+         write_val <= std_logic_vector(to_unsigned(count, 32));
+         read_1 <= std_logic_vector(to_unsigned(count, 32));
+         read_2 <= std_logic_vector(to_unsigned(count, 32));
+      end if;
+   end process;
 end Behavioral;
